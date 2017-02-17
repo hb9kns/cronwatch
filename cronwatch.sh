7ce2d1294c00b64be33d7236f1995fe184f11ec7
yargo-moni <yargo@first-monitor>
2017-02-17 14:57:51 +0100

    continue script

--- cronwatch.sh
+++ cronwatch.sh
@@ -2,15 +2,18 @@
 # update own beacon and check other published ones
 # 2017-02-17 YBonetti
 
-# address for warnings, you may put your own here -- BUT THEN DON'T COMMIT!
-wadr=${CRONWATCH-ADDR:-"john@example.com"}
+# address for warnings, you may put an external one instead of $USER
+# // BUT THEN DON'T COMMIT!
+wadr=${CRONWATCHADDR:-"$USER"}
 # address for regular reports (for no reporting, leave empty or comment out)
 radr=$wadr
 
 # standard beacon file name (only letters, numbers, dots)
 bfn=cronbeacon.html
 # local (writable) beacon path
-lbcn="$HOME/public_html/$bfn"
+lbcn=${CRONWATCHPUBDIR:-"$HOME/public_html/"}$bfn
+# local hostname for reports
+host=`hostname`
 # syntax for beacons: "MM1 URL1 MM2 URL2 ..."
 #  MM=max.age in minutes for following URL
 #  URL=beacon file to be checked (no whitespace!)
@@ -26,13 +29,25 @@ nowm=$(( `date -u +%s`/60 ))
 
 # marker for beacon epoch time (preferably non-special chars for grep/sed)
 # should be followed only by white space and epoch time on same line
-betm=':::ET:::'
+betm='==ETM=='
+
+# send stdin to address in arg.2 (or stderr if empty/missing)
+# with subject in arg.1
+sendoff () {
+ if test "$2" = ""
+# no recipients, then to stderr
+ then
+  echo "# $1" >&2
+  cat >&2
+ else cat | mail -s "$1" "$2"
+ fi
+}
 
 # update own beacon
 
 cat <<EOT >$lbcn
+`date -u` // $host
 `uptime`
-
 $betm $nowm
 EOT
 chmod a+r $lbcn
