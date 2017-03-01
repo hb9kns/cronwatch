1f2238e241e21af50d02327f406db9771f19ae8c
yargo-moni <yargo@first-monitor>
2017-03-01 13:21:14 +0100

    remove cwatchlist, because we now have a config file, and improve a lot:  - better control of sensitive data in published beacon  - clarify and improve decision process for daily warnings  - config file with not only beacon data but also parameters like    local paths and addresses

--- cronwatch.sh
+++ cronwatch.sh
@@ -1,34 +1,97 @@
 #!/bin/sh
 # update own beacon and check other published ones
-# 2017-02-28 YBonetti
 
-# address for warnings, you may put an external one instead of $USER
-# // BUT THEN DON'T PUSH TO REMOTE!
-wadr=${CRONWATCHADDR:-"$USER"}
-# address for regular reports (for no reporting, leave empty or comment out)
-radr=$wadr
+# flags:
+# suppress standard report
+quiet=no
+# send warnings daily or less often
+daily=no
+# display sensitive data
+sensitive=no
 
-# standard beacon file name (only letters, numbers, dots)
-bfn=cronbeacon.html
-# local (writable) beacon path
-lbcn=${CRONWATCHPUBDIR:-"$HOME/public_html/"}$bfn
+while test "$1" != ""
+do case $1 in
+ -q) quiet=yes ;;
+ -d) daily=yes ;;
+ -s) sensitive=yes ;;
+ *) cfgf="$1" ;;
+ esac
+ shift
+done
 
-# syntax for beacons: lines containing "MM URL"
-#  MM=max.age in minutes for following URL (0=skip)
-#  URL=beacon file to be checked (no whitespace!)
-# preferably put own beacon first, so that publishing is also checked locally
-#  (in that case, you may set very short max.age, as will first be updated)
-# DON'T PUSH IF PRODUCTION DATA PRESENT!
-bcnlist=$HOME/cwatchlist.txt
+if test "$cfgf" = ""
+then cat <<EOH
 
-if test ! -s "$bcnlist"
+usage: $0 [opt] <config>
+ (2017-03-01, YCB)
+
+options:
+ -d	only warn daily: if a beacon failed already today, don't warn again
+ -q	quiet: don't send report message for each run
+ -s	add sensitive (but helpful) data into beacon like
+	uptime, host name and current home directory
+ (In most cases, once everything works, options -d and -q are useful.)
+
+configfile contains lines with following structures:
+ BEACON mm url
+   mm=max.age in minutes for following URL (0=skip)
+   url=beacon file to be checked (no whitespace!)
+   preferably put own beacon first, to immediately check local beacon as well
+   (in that case, you may set very short max.age, as will first be updated)
+ WARN addr : email address (or user name) for warn messages
+ REPORT addr : for regular report messages (same as WARN if missing)
+ BDIR path : local writable publication path for own beacon
+ BFILE name : (inactive, see source code)
+
+EOH
+ exit
+fi
+
+if test ! -s "$cfgf"
 then cat <<EOT
 
-### beacon list '$bcnlist' unreadable or empty, aborting!
+### config file '$cfgf' unreadable or empty, aborting!
 
 EOT
  exit 1
 fi
+
+# get lines from arg.2 with case-independent prefixed arg.1, remove prefix
+# with SPC and TAB in [..] patterns
+getlines () { grep -i "^$1" "$2" | sed -e 's/[^	 ]*[ 	]*//' ; }
+
+# address for warnings, you may put an external one instead of $USER
+# to use as default // BUT THEN DON'T PUSH TO REMOTE!
+wadr=`getlines WARN "$cfgf"|tail -n 1`
+wadr=${wadr:-"$USER"}
+# address for regular reports (for no reporting, leave empty or comment out)
+radr=`getlines REPORT "$cfgf"|tail -n 1`
+# if undefined, use warn address
+radr=${radr:-"$wadr"}
+
+# standard beacon file name (only letters, numbers, dots)
+# uncomment following line, if you want to permit configurable name
+# bfn=`getlines BFILE "$cfgf"|tail -n 1`
+bfn=${bfn:-cronbeacon.html}
+# local (writable) beacon path
+lbcn=`getlines BDIR "$cfgf"|tail -n 1`
+# append file name
+lbcn=${lbcn:-"$HOME/public_html"}/$bfn
+
+# substitute variables: this allows to use $HOME, $USER etc as arguments
+# for security reasons, permit only CONTROLLED and SANITIZED input!
+eval wadr="$wadr"
+eval radr="$radr"
+eval bfn="$bfn"
+eval lbcn="$lbcn"
+
+cat <<TTT
+wadr=$wadr
+radr=$radr
+bfn=$bfn
+lbcn=$lbcn
+TTT
+exit
 
 # report files, reset them
 warn=${TEMP:-/tmp}/cronwatchwarn.txt
@@ -54,7 +117,7 @@ today=`date -u '+%Y-%m-%d'`
 # local hostname for reports
 host=`hostname`
 # timeout/sec for fetching beacon files
-tout=5
+tout=9
 # command for fetching arg.1 to stdout
 # (you may need to modify depending on the tool available)
 fetchit () { wget -q -t 2 -O - -T $tout -w $tout "$1" ; }
@@ -64,19 +127,6 @@ fetchit () { wget -q -t 2 -O - -T $tout -w $tout "$1" ; }
 # marker for beacon epoch time (only non-special chars for grep/sed, no ';')
 # should be followed only by white space and epoch time on same line
 betm='==ETM=='
-
-# flag for suppressing standard report
-quiet=no
-# flag for sending warnings daily or less often
-daily=no
-
-while test "$1" != ""
-do case $1 in
- -q) quiet=yes ;;
- -d) daily=yes ;;
- esac
- shift
-done
 
 # send stdin to address in arg.2 (or stderr if empty/missing)
 # with subject in arg.1
@@ -91,27 +141,37 @@ sendoff () {
 }
 
 # update own beacon
-# CAUTION: IN SOME CASES, HOSTNAME AND HOME DIRECTORY MAY BE PRIVACY RELEVANT!
-cat <<EOT >$lbcn
-`date -u` // $host $HOME
+# privacy relevant data must be explicitly permitted
+if test $sensitive = yes
+then cat <<EOT >>$lbcn
+// $host $HOME
 `uptime`
+`date -u`
+EOT
+else : > $lbcn
+fi
+# in any case display time stamp
+cat <<EOT >>$lbcn
 $betm $nowm
 EOT
 chmod a+r $lbcn
 
 sleep 9
 
-# process beacon list, ignore comment lines, replace '%bfn%' by $bfn
-cat $bcnlist | grep -v '^#' | sed -e "s/%bfn%/$bfn/" | {
+# process beacon list, replace '%bfn%' by $bfn
+getlines BEACON "$cfgf" | sed -e "s/%bfn%/$bfn/" | {
+# flag whether warn message must be sent
 dowarn=no
-bfails=no
+# flag whether current beacon failed
+bfail=yes
 # list of beacons that failed just now
 bhashes=''
 while read maxage remurl
+# only process beacons with positive maxage
 do if test $maxage -gt 0
  then
 # empty buffer
-  : >$tmpf
+  : > $tmpf
 # create "hash" for this beacon
   bhsh=`echo ":$today:$remurl" | tr -c '0-9:%/A-Za-z-' -`
 # and fetch beacon
@@ -128,34 +188,35 @@ do if test $maxage -gt 0
    then cat <<EOT >>$warn
 * age '$age' from '$remurl' out of range [$llim .. $maxage]
 EOT
-   bfails=yes
+   bfail=yes
    else cat <<EOT >>$rprt
 + received age '$age' from '$remurl' is ok
 EOT
+   bfail=no
    fi
-# nothing received? remote probably down
+# nothing received? remote probably down, fail for sure
   else cat <<EOT >>$warn
 * no beacon available from '$remurl' at `date -u`
 EOT
-   bfails=yes
+   bfail=yes
   fi
-# did this beacon already fail today?
-  if grep $bshs $memo >/dev/null 2>&1
-  then cat <<EOT >>$rprt
-* problem with '$remurl' today
-EOT
-# this is 1st time today
-  else if test $bfails = yes
-# any beacon fails 1st time today? warn!
-   then dowarn=yes
+
+# decide about need to warn:
+  if test $bfail = yes
 # add to list of failing beacons
-    bhashes="$bhashes
+  then bhashes="$bhashes
 $bhsh"
-   else cat <<EOT >>$rprt
-+ '$remurl' works again
+# did it already fail today?
+   if grep $bhsh $memo >/dev/null 2>&1
+# then just add remark in normal report
+   then cat <<EOT >>$rprt
+* earlier problem with '$remurl' today
 EOT
+# if 1st time today, then warn
+   else dowarn=yes
    fi
   fi
+
  fi # skip beacon if maxage<=0
 done
 
